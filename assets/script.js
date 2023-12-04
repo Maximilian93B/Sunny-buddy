077a5348b69331a57d30fea7c7548bd0dfefcae0
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2023-12-03 21:47:10 -0400

    getting fetch,get errors on console

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,10 @@ const forcastContainer = document.getElementById("current-weather");
 const searchHistory = document.getElementById("search-history");
 
 const apiKey = "96bc4821258c087bd7536d7ca06abf57"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+const apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
 
+let searchHistoryList = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-let searchHistoryList = JSON.parse(localStorage.getItem ("search-history") || [] );
 
 searchButton.addEventListener("click",function() {
     const cityName = cityInput.value.trim();
@@ -18,14 +18,70 @@ searchButton.addEventListener("click",function() {
     } fetchWeather(cityName);
 });
 
+searchHistory.addEventListener('click', function (event) {
+    if (event.target.tagName === 'li') {
+        const cityName = event.target.textContent;
+        fetchWeather(cityName);
+    }
+});
+
+
+
 function fetchWeather(cityName) {
 fetch("(`${apiUrl}/weather?q=${cityName}&appid=${apiKey}&units=metric`)")
 .then(response => response.JSON())
 .then (data => { 
     renderForcastData(data);
 
-    addToSearchHistory(cityName);
+    return fetch(`${apiUrl}/forecast?q=${cityName}&appid=${apiKey}&units=metric`);
 })
-.catch(error => console.error("Error fetching weather data:",error));
+
+.then(response => response.json())
+        .then(data => {
+            // Process the forecast data
+            renderForecastData(data);
+            // Add the city to the search history
+            addToSearchHistory(cityName);
+        })
+        .catch(error => console.error('Error fetching weather data:', error));
 }
 
+
+
+
+
+/*
+function renderWeatherData(weatherData) {
+
+    const cityNameElement = document.getElementById("city-name");
+    const dateElement = document.getElementById("date");
+    const weatherInfoElement = document.getElementById("weather-info"); 
+
+    cityNameElement.textContent = weatherData.name; 
+
+    const currentDate = new Date();
+    dateElement.textContent = currentDate.toDateString();
+
+    weatherInfoElement.innerHTML = `
+        <p>Temperature: ${weatherData.main.temp}°C</p>
+        <p>Humidity: ${weatherData.main.humidity}%</p>
+        <p>Wind Speed: ${weatherData.wind.speed} m/s</p>
+    `;
+}
+
+function renderForcastData(){
+const forcastContainer = document.getElementById ("forcast")
+
+// Make loop to run throufh forcast data --> create elemnts for each day ( 5 days )
+}
+
+
+// search history 
+
+function addToSearchHistory() {
+
+    let searchHistoryList = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+
+}
+*/
