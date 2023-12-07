ea5cfdb4db8953e7af206c4af2b4b33fc23bc856
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2023-12-07 18:16:19 -0400

    added function for render

--- assets/script.js
+++ assets/script.js
@@ -1,87 +1,72 @@
 const cityInput = document.getElementById("city-input");
 const searchButton = document.getElementById("search-button");
 const currentWeather = document.getElementById("current-weather");
-const forcastContainer = document.getElementById("current-weather");
+const forecastContainer = document.getElementById("forecast-container");
 const searchHistory = document.getElementById("search-history");
 
-const apiKey = "96bc4821258c087bd7536d7ca06abf57"
-const apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+const apiKey = "903bc0063493c0df8066d1942fcacc8f2";
+const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=903bc0063493c0df8066d1942fcacc8f2"; 
 
 let searchHistoryList = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-
-searchButton.addEventListener("click",function() {
+searchButton.addEventListener("click", function () {
     const cityName = cityInput.value.trim();
 
-    if(cityName) {
-        //fetch weather data and forcast
-    } fetchWeather(cityName);
+    if (cityName) {
+        fetchWeather(cityName);
+    }
 });
 
 searchHistory.addEventListener('click', function (event) {
-    if (event.target.tagName === 'li') {
+    if (event.target.tagName === 'LI') {
         const cityName = event.target.textContent;
         fetchWeather(cityName);
     }
 });
 
-
-
 function fetchWeather(cityName) {
-fetch("(`${apiUrl}/weather?q=${cityName}&appid=${apiKey}&units=metric`)")
-.then(response => response.JSON())
-.then (data => { 
-    renderForcastData(data);
-
-    return fetch(`${apiUrl}/forecast?q=${cityName}&appid=${apiKey}&units=metric`);
-})
-
-.then(response => response.json())
+    fetch(`${apiUrl}?q=${cityName}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
         .then(data => {
-            // Process the forecast data
+            renderWeatherData(data);
+            return fetch(`${apiUrl}/forecast?q=${cityName}&appid=${apiKey}&units=metric`);
+        })
+        .then(response => response.json())
+        .then(data => {
             renderForecastData(data);
-            // Add the city to the search history
             addToSearchHistory(cityName);
         })
         .catch(error => console.error('Error fetching weather data:', error));
 }
 
-
-
-
-
-/*
 function renderWeatherData(weatherData) {
-
     const cityNameElement = document.getElementById("city-name");
     const dateElement = document.getElementById("date");
-    const weatherInfoElement = document.getElementById("weather-info"); 
+    const weatherInfoElement = document.getElementById("weather-info");
 
-    cityNameElement.textContent = weatherData.name; 
+    cityNameElement.textContent = weatherData.name;
 
-    const currentDate = new Date();
+    const currentDate = new Date()
     dateElement.textContent = currentDate.toDateString();
-
-    weatherInfoElement.innerHTML = `
-        <p>Temperature: ${weatherData.main.temp}°C</p>
-        <p>Humidity: ${weatherData.main.humidity}%</p>
-        <p>Wind Speed: ${weatherData.wind.speed} m/s</p>
-    `;
+    
+    weatherInfoElement.innerHTML = 
+    "<p>Temperature: ${weatherData.main.temp}°C</p>"
+    "<p>Humidity: ${weatherData.main.humidity}%</p>"
+    "<p>Wind Speed: ${weatherData.wind.speed} m/s</p>"
 }
 
-function renderForcastData(){
-const forcastContainer = document.getElementById ("forcast")
+    
+    
+    // Implement the rendering of current weather data
 
-// Make loop to run throufh forcast data --> create elemnts for each day ( 5 days )
+
+
+
+function renderForecastData(forecastData) {
+    const renderForecastData = document.getElementById 
+    // Implement the rendering of forecast data
 }
 
-
-// search history 
-
-function addToSearchHistory() {
-
-    let searchHistoryList = JSON.parse(localStorage.getItem("searchHistory")) || [];
-
-
+function addToSearchHistory(cityName) {
+    // Implement adding to search history
 }
-*/
